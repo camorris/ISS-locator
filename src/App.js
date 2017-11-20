4a1a9e1d9516a4c6e7675da136248af5735f9f2e
Chase Morris <camorris@callutheran.edu>
2017-11-20 15:40:20 -0800

    deleted location and consolidating home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Link } from 'react-router-dom'
 import Home from './Home'
 import About  from './About'
 import Contact from './Contact'
-import Location from './Location'
+
 
 class App extends Component {
   render() {
@@ -14,17 +14,16 @@ class App extends Component {
 
         <div className="navigation">
           <ul>
-            <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/">ISS Location</Link> </li>
             <li> <Link to="/about">Purpose</Link> </li>
             <li> <Link to="/contact">Contact</Link></li>
-            <li><Link to="/location">ISS Location</Link></li>
           </ul>
         </div>
 
         <Route exact path='/' component={Home}/>
         <Route path='/about' component={About}/>
         <Route path='/contact' component={Contact}/>
-        <Route path='/location' component={Location}/>
+       
       </div>
     );
   }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import axios from 'axios'
 
-export default () => {
-  return(
-    <div className="Home">
-      <img src="http://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg" alt="international space station" height="500px" width="500px"/>
-      </div>  )
+class CurrentLocation extends React.Component {
+    state = {
+        loading: true,
+        currentLocation: null
+    }
+
+    componentDidMount() {
+        this.getLocation()
+        this.interval = setInterval(() => {
+            this.getLocation()
+           
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+        
+    }
+    
+    getLocation() {
+        axios({method:'get', url:'http://api.open-notify.org/iss-now.json'})
+        .then(res => this.setState({currentLocation: {
+            iss_position: {
+                latitude: res.data.iss_position.latitude,
+                longitude: res.data.iss_position.longitude
+            }
+        }}))
+    }
+
+    render() {
+        return (
+            <div className="CurrentLocation">
+                <h1>Current Location</h1>
+                <img src="http://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg" alt="international space station" height="500px" width="500px"/>
+
+                {this.state.currentLocation
+                ? `${this.state.currentLocation.iss_position.latitude}, ${this.state.currentLocation.iss_position.longitude}`
+                : null
+                }
+
+            </div>
+        )
+    }
 }
+
+export default CurrentLocation
+ 
+
+
+
+// export default () => {
+//   return(
+//     <div className="Home">
+//       <img src="http://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg" alt="international space station" height="500px" width="500px"/>
+//       </div>  )
+// }
